e52ea8617951e780e0ff1c489892d1d4129af005
leastbad <hello@leastbad.com>
2021-11-07 23:10:05 -0500

    new method no longer saves; new create method does. new destroy method. destroyed? new_record? and persisted? helper methods

--- lib/all_futures/all_futures.rb
+++ lib/all_futures/all_futures.rb
@@ -1,12 +1,12 @@
 class AllFutures < ActiveEntity::Base
-  attr_accessor :id
+  attr_accessor :id, :_redis_key, :_destroyed, :_persisted
 
   def initialize(attributes={})
     super
-    unless @id
-      @id = SecureRandom.uuid
-      save
-    end
+    @id ||= SecureRandom.uuid
+    @_redis_key = "#{self.class.name}:#{@id}"
+    @_destroyed = false
+    @_persisted = Kredis.redis.exists @_redis_key
   end
 
   def []=(attr_name, value)
@@ -15,14 +15,39 @@ class AllFutures < ActiveEntity::Base
   end
 
   def save
-    Kredis.json("#{self.class.name}:#{@id}").value = self.attributes
+    raise FrozenError.new("can't modify frozen attributes") if @_destroyed
+    @_persisted = true
+    Kredis.json(@_redis_key).value = self.attributes
     changes_applied
+    true
+  end
+
+  def destroy
+    @_destroyed = true
+    Kredis.redis.del @_redis_key if persisted?
+    self
+  end
+
+  def destroyed?
+    @_destroyed
+  end
+
+  def new_record?
+    !persisted?
+  end
+
+  def persisted?
+    @_persisted
+  end
+
+  def self.create
+    new.tap { |record| record.save }
   end
 
   def self.find(id)
-    raise ArgumentError unless id
+    raise ActiveRecord::RecordNotFound.new("Couldn't find #{name} without an ID") unless id
     json = Kredis.json("#{name}:#{id}").value
-    raise ActiveRecord::RecordNotFound unless json
+    raise ActiveRecord::RecordNotFound.new("Couldn't find #{name} with ID #{id}") unless json
     new json.merge(id: id)
   end
 end
