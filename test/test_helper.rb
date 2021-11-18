10541282c75a8f935f691b14c2be76d0ac0be881
Stephen Margheim <stephen.margheim@gmail.com>
2021-11-18 09:31:39 +0100

    Add tests with rake task

---
+++ test/test_helper.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
+require "bundler/setup"
+require "all_futures"
+require "minitest/autorun"
+
+# Configure Kredis to allow for testing without Redis in order to avoid exception: 
+# NoMethodError: undefined method `config_for' for nil:NilClass 
+Kredis.configurator = Class.new { def config_for(name) { db: "1" } end }.new
