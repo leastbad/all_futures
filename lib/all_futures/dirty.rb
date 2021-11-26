a4c14a4486336a5bec4a235e54eef4079edddcdc
leastbad <hello@leastbad.com>
2021-11-26 10:39:00 -0500

    reorganized dirty

--- lib/all_futures/dirty.rb
+++ lib/all_futures/dirty.rb
@@ -2,17 +2,42 @@
 
 module AllFutures
   module Dirty
+    def attribute_changed?(attribute)
+      _raise_unknown_attribute_error(attribute) unless attributes.key?(attribute.to_s)
+      changes.key? attribute.to_s
+    end
+    alias_method :attribute_will_change?, :attribute_changed?
+
+    def attribute_changed!(attribute)
+      _raise_unknown_attribute_error(attribute) unless attributes.key?(attribute.to_s)
+      public_send("#{attribute}_will_change!")
+    end
+
+    def clear_attribute_change(attribute)
+      _raise_unknown_attribute_error(attribute) unless attributes.key?(attribute.to_s)
+      super
+    end
+
+    def clear_attribute_changes(attr_names = changed)
+      super
+    end
+
     def dirty?
       changes.any?
     end
+    alias_method :changed_attributes?, :dirty?
 
-    def rollback_attributes(attr_names = changed)
-      attr_names.each { |attribute| _raise_unknown_attribute_error(attribute) unless attributes.key?(attribute.to_s) }.each { |attribute| rollback_attribute(attribute) }
+    def restore_attribute(attribute)
+      _raise_unknown_attribute_error(attribute) unless attributes.key?(attribute.to_s)
+      attribute = attribute.to_s
+      if attribute_changed?(attribute)
+        self[attribute] = attribute_was(attribute)
+        clear_attribute_change(attribute)
+      end
     end
 
-    def rollback_attributes!(attr_names = changed)
-      rollback_attributes(attr_names)
-      save
+    def restore_attributes(attr_names = changed)
+      attr_names.each { |attribute| _raise_unknown_attribute_error(attribute) unless attributes.key?(attribute.to_s) }.each { |attribute| restore_attribute(attribute) }
     end
 
     def rollback_attribute(attribute)
@@ -29,17 +54,13 @@ module AllFutures
       save
     end
 
-    def restore_attributes(attr_names = changed)
-      attr_names.each { |attribute| _raise_unknown_attribute_error(attribute) unless attributes.key?(attribute.to_s) }.each { |attribute| restore_attribute(attribute) }
+    def rollback_attributes(attr_names = changed)
+      attr_names.each { |attribute| _raise_unknown_attribute_error(attribute) unless attributes.key?(attribute.to_s) }.each { |attribute| rollback_attribute(attribute) }
     end
 
-    def restore_attribute(attribute)
-      _raise_unknown_attribute_error(attribute) unless attributes.key?(attribute.to_s)
-      attribute = attribute.to_s
-      if attribute_changed?(attribute)
-        self[attribute] = attribute_was(attribute)
-        clear_attribute_change(attribute)
-      end
+    def rollback_attributes!(attr_names = changed)
+      rollback_attributes(attr_names)
+      save
     end
 
     def saved_change_to_attribute?(attribute)
@@ -52,28 +73,13 @@ module AllFutures
       attribute_previously_was(attribute) != attribute_was(attribute) ? [attribute_previously_was(attribute), attribute_was(attribute)] : nil
     end
 
-    def saved_changes
-      attributes.select { |attribute| saved_change_to_attribute? attribute }
-    end
-
     def saved_changes?
       saved_changes.any?
     end
+    alias_method :previous_changes?, :saved_changes?
 
-    def attribute_changed?(attribute)
-      _raise_unknown_attribute_error(attribute) unless attributes.key?(attribute.to_s)
-      changes.key? attribute.to_s
-    end
-    alias_method :attribute_will_change?, :attribute_changed?
-
-    def attribute_changed!(attribute)
-      _raise_unknown_attribute_error(attribute) unless attributes.key?(attribute.to_s)
-      public_send("#{attribute}_will_change!")
-    end
-
-    def clear_attribute_change(attribute)
-      _raise_unknown_attribute_error(attribute) unless attributes.key?(attribute.to_s)
-      super
+    def saved_changes
+      attributes.select { |attribute| saved_change_to_attribute? attribute }
     end
   end
 end
