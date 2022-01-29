2fa5abefa9f87455d643254cb3e4c12e944e7318
leastbad <hello@leastbad.com>
2022-01-29 11:13:55 -0500

    first pass all three associations working

--- lib/all_futures/persistence.rb
+++ lib/all_futures/persistence.rb
@@ -122,36 +122,13 @@ module AllFutures
     end
 
     def touch
-      @updated_at = Time.current
+      @updated_at = Time.current.utc.to_s
     end
 
-    private
+    # treat as private
 
     def _changed_for_autosave?
       new_record? || has_changes_to_save? || marked_for_destruction? || _nested_records_changed_for_autosave?
-    end
-
-    def _create_or_update
-      _raise_readonly_record_error if readonly?
-      attributes.each_key { |attribute| _raise_readonly_attribute_error(attribute) if attr_readonly_enabled? && readonly_attribute?(attribute) && attribute_will_change?(attribute) }
-      return false if destroyed?
-
-      touch
-
-      changes_applied
-      result = new_record? ? _create_record : _update_record
-      result != false
-    end
-
-    def _create_record
-      _save_record
-      @new_record = false
-      @previously_new_record = true
-      true
-    end
-
-    def _delete_record
-      Kredis.redis.del(@redis_key)
     end
 
     def _nested_records_changed_for_autosave?
@@ -170,52 +147,181 @@ module AllFutures
       end
     end
 
-    def _save_embeds
-      _reflections.each do |embed, reflection|
-        send("_save_#{embed.macro}", embed, reflection)
+    private
+
+    def _association_foreign_key_changed?(reflection, record, key)
+      record._has_attribute?(reflection.options[:foreign_key]) && record._read_attribute(reflection.options[:foreign_key]) != key
+    end
+
+    def _associated_records_to_validate_or_save(association, new_record, autosave)
+      if new_record || _custom_validation_context?
+        association&.target
+      elsif autosave
+        association.target.find_all(&:_changed_for_autosave?)
+      else
+        association.target.find_all(&:new_record?)
       end
     end
 
-    def _save_embeds_many(embed, reflection)
-      _raise_missing_foreign_key_error(reflection) unless reflection.klass.has_attribute?(reflection.options[:foreign_key])
-      send(embed).each do |record|
-        if record.new_record?
-          record.send("#{reflection.options[:foreign_key]}=", @id)
-          record.save
+    def _association_valid?(reflection, record, index = nil)
+      return true if record.destroyed? || (reflection.options[:autosave] && record.marked_for_destruction?)
+
+      context = validation_context if _custom_validation_context?
+
+      unless (valid = record.valid?(context))
+        if reflection.options[:autosave]
+          record.errors.group_by_attribute.each do |attribute, errors|
+            errors.each do |error|
+              self.errors.import(error, attribute: "#{reflection.name}.#{attribute}")
+            end
+          end
+        else
+          errors.add(reflection.name)
         end
-        record.destroy if record.marked_for_destruction?
       end
+      valid
     end
 
-    def _save_embeds_one(embed, reflection)
-      _raise_missing_foreign_key_error(reflection) unless reflection.klass.has_attribute?(reflection.options[:foreign_key])
-      if (record = send(embed))
-        if record.new_record?
-          record.send("#{reflection.options[:foreign_key]}=", @id)
-          record.save
+    def _create_or_update
+      _raise_readonly_record_error if readonly?
+      attributes.each_key { |attribute| _raise_readonly_attribute_error(attribute) if attr_readonly_enabled? && readonly_attribute?(attribute) && attribute_will_change?(attribute) }
+      return false if destroyed?
+
+      touch
+
+      changes_applied
+
+      previously_new_record_before_save = (@new_record_before_save ||= false)
+      @new_record_before_save = !previously_new_record_before_save && new_record?
+
+      _reflections.values.select { |reflection| reflection.macro == :embedded_in }.each do |reflection|
+        send("_save_embedded_in", reflection)
+      end
+
+      result = new_record? ? _create_record : _update_record
+
+      _reflections.values.reject { |reflection| reflection.macro == :embedded_in }.each do |reflection|
+        send("_save_#{reflection.macro}", reflection)
+      end
+
+      @new_record_before_save = previously_new_record_before_save
+
+      result != false
+    end
+
+    def _create_record
+      _save_record
+      @new_record = false
+      @previously_new_record = true
+      true
+    end
+
+    def _custom_validation_context?
+      validation_context && [:create, :update].exclude?(validation_context)
+    end
+
+    def _delete_record
+      Kredis.redis.del(@redis_key)
+    end
+
+    def _record_changed?(reflection, record, key)
+      record.new_record? ||
+        _association_foreign_key_changed?(reflection, record, key) ||
+        record.will_save_change_to_attribute?(reflection.options[:foreign_key])
+    end
+
+    def _save_embeds_many(reflection)
+      if (association = association_instance_get(reflection.name))
+        _raise_missing_foreign_key_error(reflection) unless reflection.klass.has_attribute?(reflection.options[:foreign_key])
+        autosave = reflection.options[:autosave]
+
+        new_record_before_save = @new_record_before_save
+
+        if (records = _associated_records_to_validate_or_save(association, new_record_before_save, autosave))
+          if autosave
+            records_to_destroy = records.select(&:marked_for_destruction?)
+            records_to_destroy.each { |record| association.destroy(record) }
+            records -= records_to_destroy
+          end
+
+          records.each do |record|
+            next if record.destroyed?
+
+            saved = true
+
+            if autosave != false && (new_record_before_save || record.new_record?)
+              association.set_inverse_instance(record)
+
+              if autosave
+                record._write_attribute(reflection.options[:foreign_key], id)
+                saved = record.save
+              elsif !reflection.nested?
+                record._write_attribute(reflection.options[:foreign_key], id)
+                association_saved = record.save
+
+                if reflection.validate?
+                  errors.add(reflection.name) unless association_saved
+                  saved = association_saved
+                end
+              end
+            elsif autosave
+              saved = record.save
+            end
+
+            raise(AllFutures::RecordInvalid.new(association.owner)) unless saved
+          end
         end
-        record.destroy if record.marked_for_destruction?
       end
     end
 
-    def _save_embedded_in(embed, reflection)
-      if (record = send(embed))
-        record.save if record.new_record? && send(reflection.options[:foreign_key]).nil?
+    def _save_embeds_one(reflection)
+      association = association_instance_get(reflection.name)
+      _raise_missing_foreign_key_error(reflection) unless reflection.klass.has_attribute?(reflection.options[:foreign_key])
+
+      record = association&.target
+
+      if record && !record.destroyed?
+        autosave = reflection.options[:autosave]
+        if autosave && record.marked_for_destruction?
+          record.destroy
+        elsif autosave != false
+          if (autosave && record._changed_for_autosave?) || _record_changed?(reflection, record, id)
+            record._write_attribute(reflection.options[:foreign_key], id)
+            association.set_inverse_instance(record)
+            record.save
+          end
+        end
+      end
+    end
+
+    def _save_embedded_in(reflection)
+      association = association_instance_get(reflection.name)
+      return unless association&.loaded?
+
+      record = association.target
+
+      if record && !record.destroyed?
+        autosave = reflection.options[:autosave]
+
+        if autosave && record.marked_for_destruction?
+          record._write_attribute(reflection.options[:foreign_key], nil)
+          record.destroy
+        elsif autosave != false
+          saved = record.save if record.new_record? || (autosave && record.changed_for_autosave?)
+
+          saved if autosave
+        end
       end
     end
 
     def _save_record
-      _save_embeds if _reflections.any?
-      _save_version if versioning_enabled?
+      record = Kredis.json(@redis_key).value
 
-      Kredis.json(@redis_key).value = {
-        attributes: attributes,
-        created_at: created_at,
-        updated_at: touch,
-        previous_attributes: previous_attributes,
-        current_version: current_version,
-        versions: versions
-      }
+      if record&.deep_transform_keys(&:to_sym) != _snapshot
+        _save_version if versioning_enabled?
+        touch
+        Kredis.json(@redis_key).value = _snapshot
+      end
     end
 
     def _save_version
@@ -228,8 +334,28 @@ module AllFutures
       end
       @_versions[current_version] = {
         "attributes" => attributes,
-        "updated_at" => Time.current
+        "updated_at" => Time.current.utc.to_s
       }
+    end
+
+    def _snapshot
+      {
+        attributes: attributes.transform_values do |value|
+          case value
+          when ActiveSupport::TimeWithZone
+            value.utc.to_s
+          when Date
+            value.to_s
+          else
+            value
+          end
+        end,
+        created_at: created_at,
+        updated_at: @updated_at,
+        previous_attributes: previous_attributes,
+        current_version: current_version,
+        versions: versions
+      }.deep_transform_keys(&:to_sym)
     end
 
     def _raise_missing_foreign_key_error(reflection)
@@ -260,6 +386,20 @@ module AllFutures
       _save_record
       @previously_new_record = false
       true
+    end
+
+    def _validate_single_association(reflection)
+      association = association_instance_get(reflection.name)
+      record = association&.reader
+      _association_valid?(reflection, record) if record && (record._changed_for_autosave? || _custom_validation_context?)
+    end
+
+    def _validate_collection_association(reflection)
+      if (association = association_instance_get(reflection.name))
+        if (records = _associated_records_to_validate_or_save(association, new_record?, reflection.options[:autosave]))
+          records.each_with_index { |record, index| _association_valid?(reflection, record, index) }
+        end
+      end
     end
 
     module ClassMethods
