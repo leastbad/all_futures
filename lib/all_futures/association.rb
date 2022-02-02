d8e991b6ea2087a2edfc4805e9404c98cbca5e43
leastbad <hello@leastbad.com>
2022-02-02 03:41:50 -0500

    make standardrb happy

--- lib/all_futures/association.rb
+++ lib/all_futures/association.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module AllFutures
-  module Association #:nodoc:
-    def initialize_attributes(record, attributes = {}) #:nodoc:
+  module Association
+    def initialize_attributes(record, attributes = {})
       record.assign_attributes attributes if attributes.any?
       record._write_attribute(reflection.options[:foreign_key], owner.id) if owner.is_a?(AllFutures::Base)
       set_inverse_instance(record)
