8c5368b2e3421963a5a50fd179d772de5f0b2482
leastbad <hello@leastbad.com>
2026-09-18 16:28:54 -0400

    bump kredis to ~> 1.8 (fixes CVE-2023-27531 JSON deserialization); adapt test Kredis configurator and logger require for newer concurrent-ruby

--- all_futures.gemspec
+++ all_futures.gemspec
@@ -21,7 +21,7 @@ Gem::Specification.new do |gem|
   gem.add_dependency "activemodel", ">= 4.0"
   gem.add_dependency "activerecord", ">= 4.0"
   gem.add_dependency "activesupport", ">= 4.0"
-  gem.add_dependency "kredis", "~> 1.2"
+  gem.add_dependency "kredis", "~> 1.8"
   gem.add_dependency "ulid", "~> 1.3"
 
   gem.add_development_dependency "magic_frozen_string_literal", "~> 1.2.0"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 
+require "logger" # activesupport < 7.1 relies on concurrent-ruby to load this; concurrent-ruby >= 1.3.5 no longer does
 require "bundler/setup"
 require "all_futures"
 require "minitest/autorun"
@@ -17,6 +18,11 @@ Time.zone = "UTC"
 Kredis.configurator = Class.new {
   def config_for(name)
     {db: "1"}
+  end
+
+  # Kredis >= 1.3 checks for config/redis/*.yml via configurator.root
+  def root
+    Pathname.new(Dir.pwd)
   end
 }.new
 
